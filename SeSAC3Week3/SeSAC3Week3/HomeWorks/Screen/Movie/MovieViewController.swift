c59f5c79356861586f47205f1e99a26e5c634f90
김규철 <rbcjf122@gmail.com>
2023-07-28 23:49:12 +0900

    [ADD] MovieVC 구현

--- SeSAC3Week3/SeSAC3Week3/HomeWorks/Screen/Movie/MovieViewController.swift
+++ SeSAC3Week3/SeSAC3Week3/HomeWorks/Screen/Movie/MovieViewController.swift
@@ -7,20 +7,53 @@
 
 import UIKit
 
-final class MovieViewController: UIViewController {
+final class MovieViewController: BaseViewController {
 
     @IBOutlet weak var movieTableView: UITableView!
     
+    var movieList: [Movie] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setNavigator()
+        self.title = "영화 Again"
+        setTableView()
     }
     
-    
-    private func setNavigator() {
-        self.title = "영화asdsadasdas"
-        self.navigationController?.configureNavigationBarAppearance(bgColor: UIColor.tertiarySystemGroupedBackground)
-        self.navigationController?.configureLargeTitle(true)
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        getList()
     }
+    
+     func setTableView() {
+         movieTableView.backgroundColor = .tertiarySystemGroupedBackground
+        movieTableView.rowHeight = 100
+        movieTableView.register(MovieTableViewCell.self, forCellReuseIdentifier: MovieTableViewCell.CellIdentifier)
+        movieTableView.dataSource = self
+        movieTableView.delegate = self
+    }
+}
 
+extension MovieViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.movieList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: MovieTableViewCell.CellIdentifier, for: indexPath) as? MovieTableViewCell else { return UITableViewCell() }
+        
+        let row = self.movieList[indexPath.row]
+        
+        cell.configureCell(row: row)
+        
+        return cell
+    }
+}
+
+extension MovieViewController {
+    func getList() {
+        URLSessionService.shard.getMovieList(page: 5) { result in
+            self.movieList = result.Search
+            print(self.movieList)
+        }
+    }
 }
