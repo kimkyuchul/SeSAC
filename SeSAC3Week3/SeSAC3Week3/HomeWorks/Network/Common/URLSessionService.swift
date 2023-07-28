6cf0bdb0e993e53d26df9b9fa978f3a777d8eb8c
김규철 <rbcjf122@gmail.com>
2023-07-28 23:48:16 +0900

    [ADD] URLSessionService 구현 - 예외처리 및 오류처리 미반영

---
+++ SeSAC3Week3/SeSAC3Week3/HomeWorks/Network/Common/URLSessionService.swift
@@ -0,0 +1,42 @@
+//
+//  URLSessionService.swift
+//  SeSAC3Week3
+//
+//  Created by 김규철 on 2023/07/28.
+//
+
+import Foundation
+
+final class URLSessionService {
+    
+    static let shard = URLSessionService()
+    
+    private let decoder = JSONDecoder()
+    
+    func getMovieList(page: Int, completion: @escaping (MovieListResponse) -> Void) {
+        
+        guard var urlComponents = URLComponents(string: BaseURLConstant.base) else { return }
+        urlComponents.queryItems = [
+            URLQueryItem(name: "apikey", value: BaseURLConstant.apiKey),
+            URLQueryItem(name: "s", value: BaseURLConstant.search),
+            URLQueryItem(name: "type", value: BaseURLConstant.type),
+            URLQueryItem(name: "page", value: String(page))
+        ]
+        
+        guard let url = urlComponents.url else {
+            print("URL 오류")
+            return
+        }
+        
+        URLSession(configuration: .default).dataTask(with: url) {  data, response, error in
+            
+            guard let data = data, error == nil else { return }
+            
+            if let response = response as? HTTPURLResponse {
+                guard let movieData = try? self.decoder.decode(MovieListResponse.self, from: data) else { return print("파싱 오류") }
+                completion(movieData)
+            }
+            
+        }.resume()
+    }
+}
