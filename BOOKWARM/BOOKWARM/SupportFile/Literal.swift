f8751ec1def961fa209c562eb636845f072573c6
김규철 <rbcjf122@gmail.com>
2023-08-03 18:54:46 +0900

    [ADD] 리터럴 추가

---
+++ BOOKWARM/BOOKWARM/SupportFile/Literal.swift
@@ -0,0 +1,13 @@
+//
+//  Literal.swift
+//  BOOKWARM
+//
+//  Created by 김규철 on 2023/08/03.
+//
+
+import Foundation
+
+enum Literal {
+    static let placeholder = "줄거리가 없습니다.."
+    static let commentplaceholder = "나의 한줄평"
+}
