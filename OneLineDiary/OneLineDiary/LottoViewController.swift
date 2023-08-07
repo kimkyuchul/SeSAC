2968b357bb53774c0d2be36fdee80d82f9c26cba
김규철 <rbcjf122@gmail.com>
2023-08-07 12:12:06 +0900

    [ADD] 8/7 Alamofire && SwiftyJSON 네트워크 통신 기초

--- OneLineDiary/OneLineDiary/LottoViewController.swift
+++ OneLineDiary/OneLineDiary/LottoViewController.swift
@@ -7,23 +7,54 @@
 
 import UIKit
 
-class LottoViewController: UIViewController {
+import Alamofire
+import SwiftyJSON
 
+class LottoViewController: UIViewController {
+    
     
     @IBOutlet weak var textField: UITextField!
-    
+    @IBOutlet weak var datelabel: UILabel!
+    @IBOutlet weak var bonusNumberLabel: UILabel!
     let pickerView = UIPickerView()
     
-//    let list = ["김규철", "2024", "2025", "2026", "2027"]
+    let list = [1079, 1078, 1077, 1076, 1075]
     
-//    var list = Array(1...1100)
+    //    var list = Array(1...1100)
     // No exact matches in call to subscript -> list의 타입 어노테이션 설정
-    var list: [Int] = Array(1...1100).reversed()
-//    var alist = Array(repeating: "A", count: 100)
+    //    var list: [Int] = Array(1...1100).reversed()
+    //    var alist = Array(repeating: "A", count: 100)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setLayout()
+        getLottoAPI(number: 1079)
+        
+    }
+    
+    func getLottoAPI(number: Int) {
+        let url = "https://dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=\(number)"
+        AF.request(url, method: .get).validate(statusCode: 200...399).responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                let date = json["drwNoDate"].stringValue
+                let bounsNumber = json["bnusNo"].intValue
+                
+                DispatchQueue.main.async {
+                    self.datelabel.text = date
+                    self.bonusNumberLabel.text = "\(bounsNumber)"
+                }
+                
+                print(date, bounsNumber)
+                print("JSON: \(json)")
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    private func setLayout() {
         pickerView.delegate = self
         pickerView.dataSource = self
         
@@ -46,6 +77,8 @@ extension LottoViewController: UIPickerViewDelegate, UIPickerViewDataSource {
         print("\(row)")
         
         textField.text = "\(list[row])"
+        
+        getLottoAPI(number: list[row])
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
