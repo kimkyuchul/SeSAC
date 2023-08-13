6719c8ea2c5a6c53d023a60f388cfd06ad0c6ec4
김규철 <rbcjf122@gmail.com>
2023-08-14 02:11:10 +0900

    [ADD] URLConstants 구현

---
+++ TrendMedia/TrendMedia/Data/Network/Foundation/URLConstants.swift
@@ -0,0 +1,13 @@
+//
+//  URLConstants.swift
+//  TrendMedia
+//
+//  Created by 김규철 on 2023/08/13.
+//
+
+import Foundation
+
+enum URLConstants {
+    static let base = "https://api.themoviedb.org/3"
+    static let image = "https://image.tmdb.org/t/p/original/"
+}
