678efe8558a53912e05b27cb97e257c4eb52c1a1
김규철 <rbcjf122@gmail.com>
2023-10-04 14:00:06 +0900

    [ADD] 10/04 firebase Crashlytics & Analytics Basic

---
+++ SeSAC3Firebase/SeSAC3Firebase/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SeSAC3Firebase
+//
+//  Created by 김규철 on 2023/10/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let button = UIButton(type: .roundedRect)
+        button.frame = CGRect(x: 20, y: 50, width: 100, height: 30)
+        button.setTitle("Test Crash", for: [])
+        button.addTarget(self, action: #selector(crashButtonTapped(_:)), for: .touchUpInside)
+        view.addSubview(button)
+    }
+    
+    @objc func crashButtonTapped(_ sender: AnyObject) {
+        let numbers = [0]
+        let _ = numbers[1]
+    }
+}
+
---
+++ SeSAC3Firebase/SeSAC3FirebaseUITests/SeSAC3FirebaseUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SeSAC3FirebaseUITestsLaunchTests.swift
+//  SeSAC3FirebaseUITests
+//
+//  Created by 김규철 on 2023/10/04.
+//
+
+import XCTest
+
+final class SeSAC3FirebaseUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
